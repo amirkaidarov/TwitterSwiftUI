63a52f21e14182cb1abe105353ac487e680af412
amirkaidarov <KaidarovAmir1441@gmail.com>
2023-01-01 15:26:48 +0600

    Initial Commit

---
+++ TwitterSwiftUI/TwitterSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  TwitterSwiftUIApp.swift
+//  TwitterSwiftUI
+//
+//  Created by Амир Кайдаров on 1/1/23.
+//
+
+import SwiftUI
+
+@main
+struct TwitterSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
